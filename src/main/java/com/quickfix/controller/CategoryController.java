9dd7fac4d4d92b5e77c8e76df42eda3f31f83d8a
jitender2654kumar@gmail.com <142309786+JITENDERKUMAR-CODER@users.noreply.github.com>
2026-07-14 20:21:50 +0530

    Complete category management module

--- src/main/java/com/quickfix/controller/CategoryController.java
+++ src/main/java/com/quickfix/controller/CategoryController.java
@@ -6,15 +6,35 @@ import com.quickfix.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/categories")
 public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
-    @GetMapping
+    @PostMapping
     public String createCategory(@RequestBody CategoryRequest request) {
         return categoryService.createCategory(request);
+    }
+    @GetMapping
+    public List<CategoryResponse> getAllCategories() {
+        return categoryService.getAllCategories();
+
+    }
+    @GetMapping("/{id}")
+    public CategoryResponse getCategoryByid(@PathVariable Long id) {
+        return categoryService.getCategoryById(id);
+    }
+    @PutMapping("/{id}")
+    public String updateCategory(@PathVariable Long id, @RequestBody CategoryRequest request) {
+        return categoryService.updateCategory(id, request);
+    }
+    @DeleteMapping("/{id}")
+    public String deleteCategory(@PathVariable Long id) {
+        return categoryService.deleteCategory(id);
     }
 
 
--- src/main/java/com/quickfix/service/CategoryService.java
+++ src/main/java/com/quickfix/service/CategoryService.java
@@ -1,12 +1,15 @@
 package com.quickfix.service;
 
 import com.quickfix.dto.CategoryRequest;
+import com.quickfix.dto.CategoryResponse;
 import com.quickfix.entity.Category;
 import com.quickfix.repository.CategoryRepositorie;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
+
 
 @Service
 public class CategoryService {
@@ -34,4 +37,55 @@ public class CategoryService {
 
         return "Category Created Successfully";
     }
+    public List<CategoryResponse> getAllCategories() {
+        List<Category> categories = categoryRepositorie.findAll();
+        return categories.stream()
+                .map(category -> new CategoryResponse(
+                        category.getId(),
+                        category.getName(),
+                        category.getDescription(),
+                        category.getIcon()
+                ))
+                .toList();
+
+    }
+    public CategoryResponse getCategoryById(Long id) {
+
+        Category category = categoryRepositorie.findById(id)
+                .orElseThrow(() ->
+                        new RuntimeException("Category Not Found"));
+
+        return new CategoryResponse(
+                category.getId(),
+                category.getName(),
+                category.getDescription(),
+                category.getIcon()
+        );
+    }
+    public String updateCategory(Long id, CategoryRequest request) {
+    Category category = categoryRepositorie.findById(id)
+            .orElseThrow(() ->
+                    new RuntimeException("Category Not Found"));
+    if(!category.getName().equals(request.getName())
+            && categoryRepositorie.existsByName(request.getName())){
+        return "Category Already Exists";
+    }
+        category.setName(request.getName());
+        category.setDescription(request.getDescription());
+        category.setIcon(request.getIcon());
+        category.setUpdatedAt(LocalDateTime.now());
+
+        categoryRepositorie.save(category);
+
+        return "Category Updated Successfully";
+
+}
+public String deleteCategory(Long id) {
+    Category category = categoryRepositorie.findById(id)
+            .orElseThrow(() ->
+                    new RuntimeException("Category Not Found"));
+
+    categoryRepositorie.delete(category);
+    return "Category Deleted Successfully";
+}
 }
